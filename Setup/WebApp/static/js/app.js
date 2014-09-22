cdf6df0ed12b1ce360cf54b20f7f3207dd78fd3c
Rubendewit <rdw7220@gmail.com>
2014-09-22 16:30:13 +0200

    WebApp created
    webApp created

--- Setup/WebApp/static/js/app.js
+++ Setup/WebApp/static/js/app.js
@@ -1,57 +1,48 @@
 var app = app || {};
 
 (function(){
-
 	app.controller = {
 		init: function(){
-			app.router.init();					
+			app.sections.init();
+			app.router.init();
 		}
-	}
+	};
 
 	app.router = {
 		init: function(){
-			//
-			console.log("Router standby!");
 			routie({
-				'about': function() {
-					//
-					app.router.render('aboutpage');
-				},
-				'movies': function() {
-					//
-					console.log("Mooooovies.");
-				}
-			});
+    			'about': function() {
+    				app.sections.toggle('about-page');
+    			},
+
+   		 		'movies': function() {
+   		 			app.sections.toggle('movie-page');	
+    			}
+});
+		}
+	};
+
+	app.sections = {
+		init: function(){
+			this.about();
+			this.movies();
 		},
-		render: function(page){
-			if(page == "aboutpage"){
-				Transparency.render(document.getElementById('aboutpage'), app.content.about);
+		toggle: function(id){
+			var elements = document.getElementsByTagName('section');
+			for(var i in elements){
+				if(elements[i].classList)
+					elements[i].classList.remove('active');
 			}
+			if(document.getElementById(id))
+				document.getElementById(id).classList.add('active');
+		},
+		about: function(){
+			Transparency.render(document.getElementById('about-page'), app.content.about);
+		},
+		movies: function(){
+			Transparency.render(document.getElementById('movie-page'), app.content.movies, directives);
 		}
 	}
-
-	app.content = {
-		about : {
-			title: "about",
-			description: "dit is about"
-		}
-	}
-
-	
-
-	// app.about = {
-	// 	init: function(){
-	// 		//
-	// 		console.log("Welkom to the aboot!");
-	// 	}
-	// }
-
-	// app.movies = {
-	// 	init: function(){
-	// 		//
-	// 		console.log("Mooooovies.");
-	// 	}
 
 	app.controller.init();
-
 })();
